8603ed264d339ab29df4dbbe60ead2696555c22a
hempnall <hempnall@yahoo.co.uk>
2014-12-04 23:01:37 +0000

    bit better now

--- link_extractor.py
+++ link_extractor.py
@@ -1,23 +1,50 @@
 import urllib2
+#!/bin/python
+
+
 import sys
 import getopt
 from bs4 import BeautifulSoup
+
+
+def get_page(page,filter,level):
+
+	if level == 0:
+		return
+
+	try:
+		html_page = urllib2.urlopen(page)
+		soup = BeautifulSoup(html_page)
+		for link in soup.find_all('a'):
+			the_href = link.get('href')
+			if filter == '':
+				print str(the_href)
+			else:
+				if filter in the_href:
+					print str(the_href)
+				else:
+					get_page(the_href,filter,level - 1 )
+	except:
+		pass
 
 def main(argv):
 
 	url = ''
 	output_file = ''
 	filter= ''
+	levels = 1
 
 	try:
-		opts, args = getopt.getopt(argv,"hu:o:f:")
+		opts, args = getopt.getopt(argv,"hu:o:f:l:")
 	except getopt.GetoptError:
 		print 'link_extractor.py '
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
-			print 'link_extractor.py -u <url> -o <output_file>'
+			print 'link_extractor.py -u <url> -o <output_file> -f <filter> -l <recursion>'
 			sys.exit()
+		elif opt in ("-l","--levels"):
+			levels = int(arg)	
 		elif opt in ("-u", "--url"):
 			url = arg			
 		elif opt in ("-f", "--filter"):
@@ -31,16 +58,11 @@ def main(argv):
 		print 'ERROR: please specify a url'
 		sys.exit(2)
 
+
+	
 	try:
-		html_page = urllib2.urlopen(url)
-		soup = BeautifulSoup(html_page)
-		for link in soup.find_all('a'):
-			the_href = link.get('href')
-			if filter == '':
-				print str(the_href)
-			else:
-				if filter in the_href:
-					print str(the_href)
+		get_page(url,filter,levels)
+		
 
 	except Exception as inst:
 		print 'ERROR: unable to fetch URL: ' + str(inst)
