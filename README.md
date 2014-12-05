64036a46b8db90f535173536eec6ca0cb69615fb
hempnall <hempnall@yahoo.co.uk>
2014-12-05 20:09:57 +0000

    finished, i hope

---
+++ README.md
@@ -0,0 +1,16 @@
+# file downloader
+
+testset.lst:
+```
+http://www.google.co.uk
+https://www.facebook.co.uk
+http://www.wikipedia.org
+```
+
+Download all the files in testset.lst into directory outputfiles:
+`./link_extractor.py -i testset.lst -o ./output_files/`
+
+Extract all links in a web page with an (optional) filter ".co.uk":
+` ./link_extractor.py -u http://www.bbc.co.uk/ -f .co.uk`
+
+Probably best to pipe this to a file
--- link_extractor.py
+++ link_extractor.py
@@ -2,6 +2,8 @@
 
 import urllib2
 import sys
+import os
+import os.path
 import getopt
 from bs4 import BeautifulSoup
 
@@ -26,21 +28,62 @@ def get_page(page,filter,level):
 	except:
 		pass
 
+
+def download_file(input_file_name,output_directory):
+
+	try:
+
+		index_file_output_file_name = os.path.join( output_directory , "index.txt" )
+
+		ins = open( input_file_name, "r" )
+		array = []
+		for line in ins:
+		    array.append( line )
+		ins.close()
+
+		for index,url in enumerate(array):
+			try:
+				if url.strip() == '':
+					continue
+
+				file_data = urllib2.urlopen(url)
+				new_file_name = 'url_' + str(index).zfill(5) + '.bin'
+				new_complete_path_name = os.path.join(  output_directory , new_file_name )
+				output = open(new_complete_path_name,'wb')
+				output.write(file_data.read())
+				output.close()
+
+				with open(index_file_output_file_name, "a") as myfile:
+					line_to_write = new_file_name + ": " + url.strip()
+					print "INFO: " + line_to_write
+					myfile.write( line_to_write )
+
+			except Exception as inst:
+				print "ERROR: unable to fetch url [" + url.strip() + "]:" + str(inst)
+
+
+	except Exception as inst:
+		print "ERROR: could not download files:" + str(inst)
+
+
+
+
 def main(argv):
 
 	url = ''
-	output_file = ''
+	output_dir = ''
 	filter= ''
 	levels = 1
+	input_file= ''
 
 	try:
-		opts, args = getopt.getopt(argv,"hu:o:f:l:")
+		opts, args = getopt.getopt(argv,"hu:o:f:l:i:")
 	except getopt.GetoptError:
 		print 'link_extractor.py '
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
-			print 'link_extractor.py -u <url> -o <output_file> -f <filter> -l <recursion>'
+			print 'link_extractor.py -u <url> -o <output_dir> -f <filter> -l <recursion>'
 			sys.exit()
 		elif opt in ("-l","--levels"):
 			levels = int(arg)	
@@ -48,23 +91,26 @@ def main(argv):
 			url = arg			
 		elif opt in ("-f", "--filter"):
 			filter = arg
-		elif opt in ("-o", "--output-file"):
-			output_file = arg   
+		elif opt in ("-o", "--output-dir"):
+			output_dir = arg   
+		elif opt in ("-i", "--input-file"):
+			input_file = arg   						
 		else:
 			assert False, "unhandled option"      
 	
-	if url == '' :
-		print 'ERROR: please specify a url'
+	if url == ''  and input_file == '':
+		print 'ERROR: please specify a url or input file'
 		sys.exit(2)
 
 
-	
-	try:
+	if url != '':
 		get_page(url,filter,levels)
-		
+	else:	
+		if output_dir == '':	
+			download_file(input_file,os.getcwd())
+		else:
+			download_file(input_file,output_dir)	
 
-	except Exception as inst:
-		print 'ERROR: unable to fetch URL: ' + str(inst)
 
 	
 if __name__ == "__main__":
